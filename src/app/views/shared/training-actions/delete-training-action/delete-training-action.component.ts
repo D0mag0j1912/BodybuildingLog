55aa54cce99e88149cbfebf23d29e6409dcf7632
Domagoj Burić <domagoj.dev@gmail.com>
2021-11-05 19:34:16 +0100

    Added loading spinner in button and OnPush change detection

--- src/app/views/shared/training-actions/delete-training-action/delete-training-action.component.ts
+++ src/app/views/shared/training-actions/delete-training-action/delete-training-action.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { TranslateService } from '@ngx-translate/core';
@@ -17,19 +17,27 @@ export interface DeleteTrainingActionDialogData {
 @Component({
     templateUrl: './delete-training-action.component.html',
     styleUrls: ['./delete-training-action.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DeleteTrainingActionComponent {
+
+    isLoading: boolean = false;
 
     constructor(
         @Inject(MAT_DIALOG_DATA) public readonly data: DeleteTrainingActionDialogData,
         private readonly dialogRef: MatDialogRef<DeleteTrainingActionComponent>,
         private readonly translateService: TranslateService,
         private readonly snackBar: MatSnackBar,
+        private readonly changeDetectorRef: ChangeDetectorRef,
     ){}
 
     deleteTraining(trainingId: string): void {
         this.data.deleteTrainingFn(trainingId as string).pipe(
-            finalize(() => this.dialogRef.close())
+            finalize(() => {
+                this.dialogRef.close();
+                this.isLoading = false;
+                this.changeDetectorRef.markForCheck();
+            })
         ).subscribe((response: GeneralResponseData) => {
             this.snackBar.open(this.translateService.instant(response.message as string), null, {
                 duration: 3000,
