0f225f9b862ee5f9646ad6ccb04ed89a830df850
Domagoj Burić <domagoj.dev@gmail.com>
2021-11-07 12:18:55 +0100

    Removing adding hours to dates

--- src/helpers/date.helper.ts
+++ src/helpers/date.helper.ts
@@ -12,8 +12,6 @@ export function getIntervalDate(currentDate: Date): DateInterval {
     const endDate: Date = endOfWeek(currentDate, {
         weekStartsOn: 1
     });
-    startDate.setHours(startDate.getHours() + 2);
-    endDate.setHours(endDate.getHours() + 2);
     return {
         startDate: startDate,
         endDate: endDate
