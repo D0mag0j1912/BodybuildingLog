4b8b785e2c0f56935f949e06c504c674fe1d8dc3
Domagoj Burić <domagoj.dev@gmail.com>
2021-11-29 17:00:16 +0100

    Removed unnecessary code

--- src/controllers/preferences/preferences.controller.ts
+++ src/controllers/preferences/preferences.controller.ts
@@ -1,11 +1,10 @@
-import { Body, Controller, HttpException, HttpStatus, Param, Put, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Put, UseGuards } from '@nestjs/common';
 import { GeneralResponseData } from 'src/app.service';
 import { Preferences } from 'src/models/preferences/preferences.model';
 import { PreferencesService } from 'src/services/preferences/preferences.service';
 import { AuthenticationGuard } from '../../guards/authentication.guard';
 
 @Controller('preferences')
-//TODO: skontati zašto mi vraća not authenticated
 @UseGuards(AuthenticationGuard)
 export class PreferencesController {
 
@@ -18,13 +17,6 @@ export class PreferencesController {
         @Body('preferences') preferences: Preferences,
         @Param('userId') userId: string,
     ): Promise<GeneralResponseData> {
-        //TODO: izbrisati ovo kad implementiram Guard
-        if(!userId){
-            throw new HttpException({
-                status: HttpStatus.UNAUTHORIZED,
-                message: 'common.errors.not_authenticated',
-            }, HttpStatus.UNAUTHORIZED);
-        }
         return this.preferencesService.setPreferences(
             userId as string,
             preferences.language as string,
