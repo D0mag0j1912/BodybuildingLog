f6ab8976a9519c86d56c1ba0a93b9c518534f5be
Domagoj Burić <domagoj.dev@gmail.com>
2022-05-01 16:25:51 +0200

    Implementing title in reorder modal (Reorder exercises)

--- src/app/views/training/new-training/reorder-exercises/reorder-exercises.component.ts
+++ src/app/views/training/new-training/reorder-exercises/reorder-exercises.component.ts
@@ -1,8 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { DialogRoles } from '../../../../models/common/types/modal-roles.type';
 
 @Component({
     templateUrl: './reorder-exercises.component.html',
     styleUrls: ['./reorder-exercises.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ReorderExercisesComponent { }
+export class ReorderExercisesComponent {
+
+    constructor(
+        private readonly modalController: ModalController,
+    ) { }
+
+    async onCancel(): Promise<void> {
+        await this.modalController.dismiss(false, DialogRoles.CANCEL);
+    }
+}
