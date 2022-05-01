7568c00fa263c34e49a8b20b7e7f40b36d0aec08
Domagoj Burić <domagoj.dev@gmail.com>
2022-05-01 16:38:59 +0200

    Displaying current logged exercises (Reorder exercises)

--- src/app/views/training/new-training/reorder-exercises/reorder-exercises.component.ts
+++ src/app/views/training/new-training/reorder-exercises/reorder-exercises.component.ts
@@ -1,6 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { DialogRoles } from '../../../../models/common/types/modal-roles.type';
+import { Training } from '../../../../models/training/new-training/training.model';
+import { NewTrainingService } from '../../../../services/training/new-training.service';
 
 @Component({
     templateUrl: './reorder-exercises.component.html',
@@ -9,7 +13,13 @@ import { DialogRoles } from '../../../../models/common/types/modal-roles.type';
 })
 export class ReorderExercisesComponent {
 
+    readonly currentExercises$: Observable<string[]> = this.newTrainingService.currentTrainingChanged$
+        .pipe(
+            map((training: Training) => training.exercise.map(exercise => exercise.exerciseName)),
+        );
+
     constructor(
+        private readonly newTrainingService: NewTrainingService,
         private readonly modalController: ModalController,
     ) { }
 
