7c70602206f13ec81091e43b4a5ae81834f9cb85
Domagoj Burić <domagoj.dev@gmail.com>
2022-05-22 13:53:23 +0200

    [feature][BL-18] Manage units in skeleton component

--- src/app/views/shared/skeleton-loader/skeleton-loader.component.ts
+++ src/app/views/shared/skeleton-loader/skeleton-loader.component.ts
@@ -17,11 +17,28 @@ export class SkeletonLoaderComponent {
     @Input()
     borderRadius: number;
 
+    @Input()
+    pixels = true;
+
     styleSkeletonLoader(): { [key: string]: string } {
         return {
-            'width.px': `${this.width ? this.width : ''}`,
-            'height.px': `${this.height ? this.height : ''}`,
-            'border-radius.px': `${this.borderRadius ? this.borderRadius : ''}`,
+            'width': this.adjustUnit(this.width),
+            'height': this.adjustUnit(this.height),
+            'border-radius': this.adjustUnit(this.borderRadius),
         };
+    }
+
+    private adjustUnit(property: number): string {
+        if (property) {
+            if (this.pixels) {
+                return `${property}px`;
+            }
+            else {
+                return `${property}%`;
+            }
+        }
+        else {
+            return '';
+        }
     }
 }
