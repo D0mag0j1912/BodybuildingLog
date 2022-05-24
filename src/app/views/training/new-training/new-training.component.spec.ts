d116081be7ac4dd76b21d155a19090b350d3eb16
Domagoj Burić <domagoj.dev@gmail.com>
2022-05-24 18:22:11 +0200

    [feature][BL-18] Remove skeleton from new training

--- src/app/views/training/new-training/new-training.component.spec.ts
+++ src/app/views/training/new-training/new-training.component.spec.ts
@@ -1,4 +1,4 @@
-import {HttpClientTestingModule} from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NewTrainingComponent } from './new-training.component';
