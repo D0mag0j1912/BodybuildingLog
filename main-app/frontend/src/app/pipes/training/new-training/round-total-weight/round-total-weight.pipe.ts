c09a452b6dba1e66a7506579789f8d702f645547
Domagoj Burić <domagoj.dev@gmail.com>
2022-07-11 18:32:03 +0200

    [feature][BL-92] Set metric in input

--- main-app/frontend/src/app/pipes/training/new-training/round-total-weight/round-total-weight.pipe.ts
+++ main-app/frontend/src/app/pipes/training/new-training/round-total-weight/round-total-weight.pipe.ts
@@ -1,12 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DEFAULT_WEIGHT_FORMAT } from '../../../../constants/shared/default-weight-format.const';
 
-@Pipe({
-    name: 'roundTotalWeight',
-})
+@Pipe({ name: 'roundTotalWeight' })
 export class RoundTotalWeightPipe implements PipeTransform {
 
     transform(totalWeight: number): string {
         return totalWeight ? `${(Math.round(totalWeight * 100) / 100).toString()} ${DEFAULT_WEIGHT_FORMAT}` : `0 ${DEFAULT_WEIGHT_FORMAT}`;
     }
+
 }
