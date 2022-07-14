6423a824a64a2fdce4aa33192df061e4d28213a3
Domagoj Burić <domagoj.dev@gmail.com>
2022-07-14 19:23:05 +0200

    [feature][BL-94] Remove languages and unit cmps

--- main-app/frontend/src/app/modules/navigation.module.ts
+++ main-app/frontend/src/app/modules/navigation.module.ts
@@ -4,15 +4,11 @@ import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { PreferencesComponent } from '../views/navigation/preferences/preferences.component';
-import { LanguagesComponent } from '../views/navigation/side-nav/languages/languages.component';
 import { SideNavComponent } from '../views/navigation/side-nav/side-nav.component';
-import { UnitsComponent } from '../views/navigation/side-nav/units/units.component';
 import { AuthModule } from './auth/auth.module';
 
 const COMPONENTS = [
     SideNavComponent,
-    LanguagesComponent,
-    UnitsComponent,
     PreferencesComponent,
 ];
 
