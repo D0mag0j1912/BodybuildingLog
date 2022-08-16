6203220bbb35002d2d087965ecdb2c2ce0c3830a
Domagoj Burić <domagoj.dev@gmail.com>
2022-08-16 18:55:01 +0200

    [feature][BL-112] Refactor signup form

--- main-app/frontend/src/app/views/auth/signup/signup.component.ts
+++ main-app/frontend/src/app/views/auth/signup/signup.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
-import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
-import { EMPTY } from 'rxjs';
-import { catchError, finalize, takeUntil } from 'rxjs/operators';
+import { BehaviorSubject, EMPTY, Observable } from 'rxjs';
+import { catchError, finalize } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { MESSAGE_DURATION } from '../../../constants/shared/message-duration.const';
 import { LanguageCode, WeightUnit } from '../../../models/common/preferences.type';
@@ -10,101 +10,88 @@ import { AuthService } from '../../../services/api/auth/auth.service';
 import { SignupService } from '../../../services/api/auth/signup.service';
 import { LoadingControllerService } from '../../../services/shared/loading-controller.service';
 import { ToastControllerService } from '../../../services/shared/toast-controller.service';
-import { UnsubscribeService } from '../../../services/shared/unsubscribe.service';
 import * as AuthCustomValidators from '../../../validators/auth/auth.validators';
 import { IonFocusDurations } from '../../../constants/shared/ion-focus-durations.const';
 import { DEFAULT_WEIGHT_UNIT } from '../../../constants/shared/default-weight-format.const';
+import { DEFAULT_LANGUAGE } from '../../../constants/shared/default-language.const';
 
-type FormData = {
-    language?: LanguageCode;
-    weightUnit?: WeightUnit;
-    email?: string;
-    password?: string;
-    confirmPassword?: string;
-};
 @Component({
     selector: 'bl-signup',
     templateUrl: './signup.component.html',
     styleUrls: ['./signup.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
-    providers: [UnsubscribeService],
 })
-export class SignupComponent {
+export class SignupComponent implements OnDestroy {
 
-    isLoading = false;
+    private readonly _isLoading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+    readonly isLoading$: Observable<boolean> = this._isLoading$.asObservable();
 
-    form: UntypedFormGroup;
+    form = new FormGroup({
+        language: new FormControl<LanguageCode>(DEFAULT_LANGUAGE, [Validators.required]),
+        weightUnit: new FormControl<WeightUnit>(DEFAULT_WEIGHT_UNIT, [Validators.required]),
+        email: new FormControl('', {
+            validators: [Validators.required, Validators.email],
+            asyncValidators: [AuthCustomValidators.isEmailAvailable(this._signupService, this._changeDetectorRef)],
+        }),
+        password: new FormControl('', [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20)],
+        ),
+        confirmPassword: new FormControl('', [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20)],
+        ),
+    }, { validators: AuthCustomValidators.samePasswords() });
 
     constructor(
-        private readonly authService: AuthService,
-        private readonly signupService: SignupService,
-        private readonly translateService: TranslateService,
-        private readonly unsubscribeService: UnsubscribeService,
-        private readonly loadingControllerService: LoadingControllerService,
-        private readonly toastControllerService: ToastControllerService,
-        private readonly changeDetectorRef: ChangeDetectorRef,
-        private readonly router: Router,
-    ) {
-        this.form = new UntypedFormGroup({
-            language: new UntypedFormControl('en', [Validators.required]),
-            weightUnit: new UntypedFormControl(DEFAULT_WEIGHT_UNIT, [Validators.required]),
-            email: new UntypedFormControl(null, {
-                validators: [Validators.required, Validators.email],
-                asyncValidators: [AuthCustomValidators.isEmailAvailable(this.signupService, this.changeDetectorRef)],
-            }),
-            password: new UntypedFormControl(null, [
-                Validators.required,
-                Validators.minLength(6),
-                Validators.maxLength(20)],
-            ),
-            confirmPassword: new UntypedFormControl(null, [
-                Validators.required,
-                Validators.minLength(6),
-                Validators.maxLength(20)],
-            ),
-        }, { validators: AuthCustomValidators.samePasswords() });
-    }
+        private readonly _authService: AuthService,
+        private readonly _signupService: SignupService,
+        private readonly _translateService: TranslateService,
+        private readonly _loadingControllerService: LoadingControllerService,
+        private readonly _toastControllerService: ToastControllerService,
+        private readonly _changeDetectorRef: ChangeDetectorRef,
+        private readonly _router: Router,
+    ) { }
 
     get focusDuration(): number {
         return IonFocusDurations.SIGNUP;
+    }
+
+    ngOnDestroy(): void {
+        this._isLoading$.complete();
     }
 
     async onSubmit(): Promise<void> {
         if (!this.form.valid) {
             return;
         }
-        this.isLoading = true;
-        await this.loadingControllerService.displayLoader({ message: 'auth.signing_in' });
+        this._isLoading$.next(true);
+        await this._loadingControllerService.displayLoader({ message: 'auth.signing_in' });
 
-        this.authService.signup(
-            this.accessFormData('language').value as LanguageCode,
-            this.accessFormData('weightUnit').value as WeightUnit,
-            this.accessFormData('email').value,
-            this.accessFormData('password').value,
-            this.accessFormData('confirmPassword').value,
+        this._authService.signup(
+            this.form.get('language').value,
+            this.form.get('weightUnit').value,
+            this.form.get('email').value,
+            this.form.get('password').value,
+            this.form.get('confirmPassword').value,
         ).pipe(
             catchError(_ => EMPTY),
             finalize(async () => {
-                this.isLoading = false;
-                await this.loadingControllerService.dismissLoader();
-                this.changeDetectorRef.markForCheck();
+                this._isLoading$.next(false);
+                await this._loadingControllerService.dismissLoader();
             }),
-            takeUntil(this.unsubscribeService),
         )
         .subscribe(async response => {
             if (response.Success) {
-                await this.toastControllerService.displayToast({
-                    message: this.translateService.instant(response.Message),
+                await this._toastControllerService.displayToast({
+                    message: this._translateService.instant(response.Message),
                     duration: MESSAGE_DURATION.GENERAL,
                     color: response.Success ? 'primary' : 'danger',
                 });
-                await this.router.navigate(['/auth/login']);
+                await this._router.navigate(['/auth/login']);
             }
         });
     }
-
-    accessFormData(formFieldName: keyof FormData): UntypedFormControl {
-        return this.form.get(formFieldName) as UntypedFormControl;
-    }
-
 }
