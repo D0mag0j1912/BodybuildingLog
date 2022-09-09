e3438ee566dbfada2c04834b9938b7993a37ab74
Domagoj Burić <domagoj.dev@gmail.com>
2022-09-09 19:02:24 +0200

    [feature][BL-133] Rename pipe class

--- main-app/frontend/src/app/pipes/training/new-training/filter-available-exercises.pipe.ts
+++ main-app/frontend/src/app/pipes/training/new-training/filter-available-exercises.pipe.ts
@@ -6,11 +6,11 @@ import { NewTraining } from '../../../models/training/new-training/new-training.
 import { NewTrainingStoreService } from '../../../services/store/training/new-training-store.service';
 
 @Pipe({ name: 'filterAvailableExercises' })
-export class NewTrainingPipe implements PipeTransform {
-    constructor(private readonly newTrainingStoreService: NewTrainingStoreService) {}
+export class FilterAvailableExercisesPipe implements PipeTransform {
+    constructor(private readonly _newTrainingStoreService: NewTrainingStoreService) {}
 
     transform(_value: Observable<Exercise[]>, index: number): Observable<Exercise[]> {
-        return this.newTrainingStoreService.currentTrainingChanged$.pipe(
+        return this._newTrainingStoreService.currentTrainingChanged$.pipe(
             take(1),
             map((training: NewTraining) => training.exercises[index]?.availableExercises ?? []),
         );
