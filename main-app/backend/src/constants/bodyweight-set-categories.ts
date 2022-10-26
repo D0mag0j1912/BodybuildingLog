1cef9e103509330b67ff5807297a7a055df8f325
Domagoj Burić <domagoj.dev@gmail.com>
2022-10-26 15:36:33 +0200

    [feature][BL-135] Remove bodyweight set categories

--- main-app/backend/src/constants/bodyweight-set-categories.ts
+++
@@ -1,8 +0,0 @@
-import { SetCategoryType } from '../models/training/new-training/set.model';
-
-export const BODYWEIGHT_SET_CATEGORIES: SetCategoryType[] = [
-    'dynamicBodyweight',
-    'dynamicWeighted',
-    'staticBodyweight',
-    'staticWeighted',
-];
