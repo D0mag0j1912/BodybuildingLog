387da6907f5eb93a593b3a67965d3bb35f87773f
Domagoj Burić <domagoj.dev@gmail.com>
2022-11-24 17:38:07 +0100

    [feature][BL-128] Apply md styles

--- main-app/frontend/capacitor.config.ts
+++ main-app/frontend/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
     appId: 'io.ionic.starter',
-    appName: 'trainingApk',
+    appName: 'bodybuildingLog',
     webDir: 'www',
     bundledWebRuntime: false,
 };
