d4b7eb8d013f45ed20f69770b354fbd3cc30488a
Domagoj Burić <domagoj.dev@gmail.com>
2023-01-26 17:38:50 +0100

    [feature][BL-160] Create training splits list cmp

---
+++ main-app/frontend/src/app/views/training/training-splits/training-splits-list/training-splits-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'bl-training-splits-list',
+    templateUrl: './training-splits-list.component.html',
+    styleUrls: ['./training-splits-list.component.scss'],
+})
+export class TrainingSplitsListComponent {}
--- main-app/frontend/src/app/views/training/training-splits/training-splits.module.ts
+++ main-app/frontend/src/app/views/training/training-splits/training-splits.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { TrainingSplitsListComponent } from './training-splits-list/training-splits-list.component';
 import { TrainingSplitsRoutingModule } from './training-splits-routing.module';
 import { TrainingSplitsSearchComponent } from './training-splits-search/training-splits-search.component';
 import { TrainingSplitsComponent } from './training-splits.component';
 
-const COMPONENTS = [TrainingSplitsComponent, TrainingSplitsSearchComponent];
+const COMPONENTS = [
+    TrainingSplitsComponent,
+    TrainingSplitsSearchComponent,
+    TrainingSplitsListComponent,
+];
 
 const IMPORTS = [TrainingSplitsRoutingModule];
 
