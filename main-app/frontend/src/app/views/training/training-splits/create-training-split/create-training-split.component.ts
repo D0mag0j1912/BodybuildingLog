57f94f69c3c863153d4df1f80a4ff8f190f34fdd
Domagoj Burić <domagoj.dev@gmail.com>
2023-01-31 18:44:11 +0100

    [feature][BL-162] Create modal title

--- main-app/frontend/src/app/views/training/training-splits/create-training-split/create-training-split.component.ts
+++ main-app/frontend/src/app/views/training/training-splits/create-training-split/create-training-split.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { DialogRoles } from '../../../../constants/enums/dialog-roles.enum';
 
 @Component({
     templateUrl: './create-training-split.component.html',
     styleUrls: ['./create-training-split.component.scss'],
 })
-export class CreateTrainingSplitComponent {}
+export class CreateTrainingSplitComponent {
+    constructor(private _modalController: ModalController) {}
+
+    async onCancel(): Promise<void> {
+        await this._modalController.dismiss(false, DialogRoles.CANCEL);
+    }
+}
