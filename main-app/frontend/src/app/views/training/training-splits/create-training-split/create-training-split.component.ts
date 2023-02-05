88ce0f2495e880466ccea6762db297246371fb72
Domagoj Burić <domagoj.dev@gmail.com>
2023-02-05 11:17:40 +0100

    [feature][BL-162] Call exercises API

--- main-app/frontend/src/app/views/training/training-splits/create-training-split/create-training-split.component.ts
+++ main-app/frontend/src/app/views/training/training-splits/create-training-split/create-training-split.component.ts
@@ -4,16 +4,33 @@ import { ModalController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { ExerciseDto } from '../../../../../api';
 import { DialogRoles } from '../../../../constants/enums/dialog-roles.enum';
+import { mapStreamData } from '../../../../helpers/training/past-trainings/map-stream-data.helper';
+import { StreamData } from '../../../../models/common/common.model';
+import { ExercisesService } from '../../../../services/api/training/exercises.service';
 
 @Component({
     templateUrl: './create-training-split.component.html',
     styleUrls: ['./create-training-split.component.scss'],
 })
 export class CreateTrainingSplitComponent {
-    readonly daysOfWeek$: Observable<string[]> = this._translateService
+    daysOfWeek$: Observable<string[]> = this._translateService
         .stream('weekdays')
         .pipe(map((value) => Object.values(value)));
+
+    exercisesData$ = this._exercisesService.getExercises().pipe(
+        mapStreamData(),
+        map((data: StreamData<ExerciseDto[]>) => {
+            if (data.IsError) {
+                return {
+                    ...data,
+                    Value: [],
+                };
+            }
+            return data;
+        }),
+    );
 
     form = new FormGroup({
         Name: new FormControl('', Validators.required),
@@ -22,6 +39,7 @@ export class CreateTrainingSplitComponent {
     activeDay = 1;
 
     constructor(
+        private _exercisesService: ExercisesService,
         private _translateService: TranslateService,
         private _modalController: ModalController,
     ) {}
