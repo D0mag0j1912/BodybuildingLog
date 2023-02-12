5e67540496473f5b14f3678b264fd8a9992de22b
Domagoj Burić <domagoj.dev@gmail.com>
2023-02-12 18:28:14 +0100

    [feature][BL-162] React to exercises value changes

--- main-app/frontend/src/app/views/training/training-splits/create-training-split/create-training-split.component.ts
+++ main-app/frontend/src/app/views/training/training-splits/create-training-split/create-training-split.component.ts
@@ -1,22 +1,41 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { Observable, of } from 'rxjs';
-import { map, switchMap, take } from 'rxjs/operators';
+import { map, switchMap, take, tap } from 'rxjs/operators';
 import { ExerciseDto as Exercise } from '../../../../../api/models/exercise-dto';
 import { DialogRoles } from '../../../../constants/enums/dialog-roles.enum';
 import { mapStreamData } from '../../../../helpers/training/past-trainings/map-stream-data.helper';
 import { StreamData } from '../../../../models/common/common.model';
 import { ExercisesService } from '../../../../services/api/training/exercises.service';
 import { ExercisesStoreService } from '../../../../services/store/training/exercises-store.service';
-import { EXERCISE_MOCK } from '../../../../mock/training-split.mock';
 
 @Component({
     templateUrl: './create-training-split.component.html',
     styleUrls: ['./create-training-split.component.scss'],
 })
 export class CreateTrainingSplitComponent {
+    form = new FormGroup({
+        Name: new FormControl('', Validators.required),
+        Exercises: new FormControl<Exercise[]>([], Validators.required),
+        NumberOfSets: new FormArray<FormControl<number>>([]),
+    });
+    activeDay = 1;
+
+    exercisesChanges$ = this.form.controls.Exercises.valueChanges.pipe(
+        tap((selectedExercises: Exercise[]) => {
+            while (this.form.controls.NumberOfSets.length !== 0) {
+                this.form.controls.NumberOfSets.removeAt(0);
+            }
+            [...selectedExercises].forEach((exercise: Exercise) => {
+                this.form.controls.NumberOfSets.push(
+                    new FormControl(exercise.numberOfSets, Validators.required),
+                );
+            });
+        }),
+    );
+
     daysOfWeek$: Observable<string[]> = this._translateService
         .stream('weekdays')
         .pipe(map((value) => Object.values(value)));
@@ -41,12 +60,6 @@ export class CreateTrainingSplitComponent {
             return data;
         }),
     );
-
-    form = new FormGroup({
-        Name: new FormControl('', Validators.required),
-        Exercises: new FormControl<Exercise[]>(EXERCISE_MOCK, Validators.required),
-    });
-    activeDay = 1;
 
     constructor(
         private _exercisesService: ExercisesService,
