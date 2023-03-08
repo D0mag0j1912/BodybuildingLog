ca7397bd3c467772ab6b6d0c8e24332510622580
Domagoj Burić <domagoj.dev@gmail.com>
2023-03-08 19:33:44 +0100

    [feature][BL-162] Differentiate week day exercises

--- main-app/frontend/src/app/views/shared/searchable-select/searchable-select.component.ts
+++ main-app/frontend/src/app/views/shared/searchable-select/searchable-select.component.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Component, forwardRef, Input } from '@angular/core';
+import { Component, forwardRef, Input, SimpleChanges } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { SearchbarCustomEvent } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
@@ -30,13 +30,7 @@ export class SearchableSelectComponent implements ControlValueAccessor {
     title: string;
 
     @Input()
-    set data(data: any[]) {
-        this._data = data;
-    }
-    get data(): any[] {
-        return this._data;
-    }
-    private _data: any[];
+    data: any[];
 
     @Input()
     multiple = false;
@@ -48,6 +42,9 @@ export class SearchableSelectComponent implements ControlValueAccessor {
     visibleValue: string;
 
     @Input()
+    hiddenValue: string;
+
+    @Input()
     panelClass: string;
 
     constructor(private _translateService: TranslateService) {}
@@ -55,9 +52,14 @@ export class SearchableSelectComponent implements ControlValueAccessor {
     writeValue(selectedItems: any[]): void {
         this.selected = selectedItems;
         this.filtered = this.data.map((item) => {
-            const foundItem = this.selected.find((selectedItem) => selectedItem._id === item._id);
+            const foundItem = this.selected.find(
+                (selectedItem) => selectedItem[this.hiddenValue] === item[this.hiddenValue],
+            );
             if (foundItem) {
-                item.selected = true;
+                return {
+                    ...item,
+                    selected: true,
+                };
             }
             return item;
         });
@@ -73,6 +75,16 @@ export class SearchableSelectComponent implements ControlValueAccessor {
 
     open(): void {
         this.isOpen = true;
+        this.filtered = this.filtered.map((value: any) => {
+            const itemFound = this.selected.find(
+                (selectedValue: any) => selectedValue[this.hiddenValue] === value[this.hiddenValue],
+            );
+            if (!itemFound) {
+                const { selected, ...rest } = value;
+                return rest;
+            }
+            return value;
+        });
     }
 
     cancel(): void {
@@ -104,6 +116,8 @@ export class SearchableSelectComponent implements ControlValueAccessor {
             this.selected = this.data.filter((item) => item.selected);
             this.onChange(this.selected);
             this.isOpen = false;
+        } else {
+            this.data = [...this.filtered];
         }
     }
 }
