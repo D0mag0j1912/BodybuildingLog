090b47fa0c9cbaf30f84579c17f16dd671c0e1fd
Domagoj Burić <domagoj.dev@gmail.com>
2023-06-02 18:33:22 +0200

    [feature][BL-176] Emit selected training split

--- main-app/frontend/src/app/views/training/training-splits/training-split/training-split.component.ts
+++ main-app/frontend/src/app/views/training/training-splits/training-split/training-split.component.ts
@@ -34,7 +34,9 @@ export class TrainingSplitComponent {
         this.deleteTrainingSplit.emit(this.trainingSplit._id);
     }
 
-    useTrainingSplit(): void {
-        this.emitTrainingSplit.emit(this.trainingSplit);
+    onSelectedTrainingSplitChange(isChecked: boolean): void {
+        if (isChecked) {
+            this.emitTrainingSplit.emit(this.trainingSplit);
+        }
     }
 }
