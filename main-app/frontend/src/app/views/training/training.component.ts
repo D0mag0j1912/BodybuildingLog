069fbf4dc0c097f8f5916a4122e50e792bf0c880
Domagoj Burić <domagoj.dev@gmail.com>
2023-07-17 19:51:56 +0200

    [fix][BL-173] Navigate to past trainings

--- main-app/frontend/src/app/views/training/training.component.ts
+++ main-app/frontend/src/app/views/training/training.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { endOfDay, endOfWeek, format, startOfDay, startOfWeek } from 'date-fns';
 import { QUERY_PARAMS_DATE_FORMAT } from '../../constants/training/past-trainings-date-format.const';
-import { PastTrainingsQueryParams } from '../../models/training/past-trainings/past-trainings.model';
 import { PreferencesStoreService } from '../../services/store/shared/preferences-store.service';
 
 @Component({
@@ -11,23 +9,16 @@ import { PreferencesStoreService } from '../../services/store/shared/preferences
     styleUrls: ['./training.component.scss'],
 })
 export class TrainingComponent {
-    constructor(
-        private _preferencesStoreService: PreferencesStoreService,
-        private _router: Router,
-    ) {}
-
-    async goToPastTrainings(): Promise<void> {
-        const showByPeriod = this._preferencesStoreService.getPreferences()?.showByPeriod ?? 'week';
-        const startDate = startOfWeek(startOfDay(new Date()), { weekStartsOn: 1 });
-        const endDate =
-            showByPeriod === 'week'
-                ? endOfWeek(endOfDay(new Date()), { weekStartsOn: 1 })
-                : startOfWeek(startOfDay(new Date()), { weekStartsOn: 1 });
-        const queryParams = {
-            startDate: format(startDate, QUERY_PARAMS_DATE_FORMAT),
-            endDate: format(endDate, QUERY_PARAMS_DATE_FORMAT),
-            showBy: showByPeriod,
-        } as Partial<PastTrainingsQueryParams>;
-        await this._router.navigate(['/training/tabs/past-trainings'], { queryParams });
-    }
+    showByPeriod = this._preferencesStoreService.getPreferences()?.showByPeriod ?? 'week';
+    startDate = format(
+        startOfWeek(startOfDay(new Date()), { weekStartsOn: 1 }),
+        QUERY_PARAMS_DATE_FORMAT,
+    );
+    endDate = format(
+        this.showByPeriod === 'week'
+            ? endOfWeek(endOfDay(new Date()), { weekStartsOn: 1 })
+            : startOfWeek(startOfDay(new Date()), { weekStartsOn: 1 }),
+        QUERY_PARAMS_DATE_FORMAT,
+    );
+    constructor(private _preferencesStoreService: PreferencesStoreService) {}
 }
