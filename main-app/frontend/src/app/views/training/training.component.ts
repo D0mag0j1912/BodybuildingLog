f135c0e722ae58f0b20bf07e1ea9f8c265726255
Domagoj Burić <domagoj.dev@gmail.com>
2023-07-18 18:05:55 +0200

    [fix][BL-150] React to change in preferences

--- main-app/frontend/src/app/views/training/training.component.ts
+++ main-app/frontend/src/app/views/training/training.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { endOfDay, endOfWeek, format, startOfDay, startOfWeek } from 'date-fns';
+import { map } from 'rxjs/operators';
 import { QUERY_PARAMS_DATE_FORMAT } from '../../constants/training/past-trainings-date-format.const';
 import { PreferencesStoreService } from '../../services/store/shared/preferences-store.service';
+import { PreferencesDto as Preferences } from '../../../api/models/preferences-dto';
 
 @Component({
     selector: 'bl-training',
@@ -9,16 +11,22 @@ import { PreferencesStoreService } from '../../services/store/shared/preferences
     styleUrls: ['./training.component.scss'],
 })
 export class TrainingComponent {
-    showByPeriod = this._preferencesStoreService.getPreferences()?.showByPeriod ?? 'week';
+    showByPeriod$ = this._preferencesStoreService.preferencesChanged$.pipe(
+        map((preferences: Preferences) => preferences.showByPeriod),
+    );
     startDate = format(
         startOfWeek(startOfDay(new Date()), { weekStartsOn: 1 }),
         QUERY_PARAMS_DATE_FORMAT,
     );
-    endDate = format(
-        this.showByPeriod === 'week'
-            ? endOfWeek(endOfDay(new Date()), { weekStartsOn: 1 })
-            : startOfWeek(startOfDay(new Date()), { weekStartsOn: 1 }),
-        QUERY_PARAMS_DATE_FORMAT,
+    endDate$ = this.showByPeriod$.pipe(
+        map((showByPeriod: Preferences['showByPeriod']) =>
+            format(
+                showByPeriod === 'week'
+                    ? endOfWeek(endOfDay(new Date()), { weekStartsOn: 1 })
+                    : startOfWeek(startOfDay(new Date()), { weekStartsOn: 1 }),
+                QUERY_PARAMS_DATE_FORMAT,
+            ),
+        ),
     );
     constructor(private _preferencesStoreService: PreferencesStoreService) {}
 }
