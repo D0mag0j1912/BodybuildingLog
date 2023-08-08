689bf0e3fc9e451f9b8a3aa5735602abaa3b9913
Domagoj Burić <domagoj.dev@gmail.com>
2023-08-08 17:00:37 +0200

    [feature][BL-191] Save filter to the store

--- main-app/frontend/src/app/views/training/training.component.ts
+++ main-app/frontend/src/app/views/training/training.component.ts
@@ -1,30 +1,29 @@
 import { Component } from '@angular/core';
 import { endOfDay, endOfWeek, format, startOfDay, startOfWeek } from 'date-fns';
-import { takeUntil } from 'rxjs/operators';
+import { take } from 'rxjs/operators';
 import { QUERY_PARAMS_DATE_FORMAT } from '../../constants/training/past-trainings-date-format.const';
 import { PreferencesStoreService } from '../../services/store/shared/preferences-store.service';
 import { PreferencesDto as Preferences } from '../../../api/models/preferences-dto';
 import { PastTrainingsQueryParams } from '../../models/training/past-trainings/past-trainings.model';
-import { UnsubscribeService } from '../../services/shared/unsubscribe.service';
 import { encodeFilter } from '../../helpers/encode-decode.helper';
+import { PastTrainingsFacadeService } from '../../store/past-trainings/past-trainings-facade.service';
 
 @Component({
     selector: 'bl-training',
     templateUrl: './training.component.html',
     styleUrls: ['./training.component.scss'],
-    providers: [UnsubscribeService],
 })
 export class TrainingComponent {
     filter: string;
 
     constructor(
         private _preferencesStoreService: PreferencesStoreService,
-        private _unsubscribeService: UnsubscribeService,
+        private _pastTrainingsFacadeService: PastTrainingsFacadeService,
     ) {}
 
     ionViewWillEnter(): void {
         this._preferencesStoreService.preferencesChanged$
-            .pipe(takeUntil(this._unsubscribeService))
+            .pipe(take(1))
             .subscribe((preferences: Preferences) => {
                 const showByPeriod = preferences.showByPeriod;
                 const queryParams: PastTrainingsQueryParams = {
@@ -39,5 +38,9 @@ export class TrainingComponent {
                 };
                 this.filter = encodeFilter(queryParams);
             });
+    }
+
+    saveFilter(): void {
+        this._pastTrainingsFacadeService.saveFilter(this.filter);
     }
 }
