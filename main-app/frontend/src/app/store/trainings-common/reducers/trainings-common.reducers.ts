136b55657d13b0fb8c9cc0a9c178cca27bd9909c
Domagoj Burić <domagoj.dev@gmail.com>
2023-08-24 16:11:52 +0200

    [fix][BL-192] Get exercises

--- main-app/frontend/src/app/store/trainings-common/reducers/trainings-common.reducers.ts
+++ main-app/frontend/src/app/store/trainings-common/reducers/trainings-common.reducers.ts
@@ -1,6 +1,7 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
 import { ExerciseDto as Exercise } from '../../../../api/models/exercise-dto';
 import { StreamData } from '../../../models/common/common.model';
+import * as TrainingsCommonActions from '../actions/trainings-common.actions';
 
 export interface TrainingsCommonState {
     exercises: StreamData<Exercise[]>;
@@ -10,4 +11,10 @@ export const initialTrainingsCommonState: TrainingsCommonState = {
     exercises: undefined,
 };
 
-export const trainingsCommonReducer = createReducer(initialTrainingsCommonState);
+export const trainingsCommonReducer = createReducer(
+    initialTrainingsCommonState,
+    on(TrainingsCommonActions.setExercises, (state, { exercises }) => ({
+        ...state,
+        exercises,
+    })),
+);
